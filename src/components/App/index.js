947f473c71032ca88223c5e0e0653d845c8e15c7
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-11-09 17:23:08 -0600

    Initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>INSIDE APP COMPONENT</h2>
+        <button className="btn btn-info">I am a button</button>
+      </div>
+    );
+  }
+}
+
+export default App;
