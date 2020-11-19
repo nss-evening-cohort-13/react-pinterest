c39048b09d19f5c8648e66f537736a499b83c3e3
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-11-19 11:34:57 -0600

    added comments and updates to route

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -9,10 +9,17 @@ import SingleBoard from '../views/SingleBoard';
 import NotFound from '../views/NotFound';
 import SearchResults from '../views/SearchResults';
 
+// this function is creating a private route and returing the specified route based on the props
+
+// We specify the specific props we want to use in the routeChecker and pass the rest with the spread
 const PrivateRoute = ({ component: Component, user, ...rest }) => {
-  const routeChecker = (props) => (user
-    ? (<Component {...props} />)
-    : (<Redirect to={{ pathname: '/', state: { from: props.location } }} />));
+  // when we call this function in the return, it is looking for an argument. `props` here is taco.
+  const routeChecker = (taco) => (user
+    ? (<Component {...taco} />)
+    : (<Redirect to={{ pathname: '/', state: { from: taco.location } }} />));
+    // this render method is one we can use instead of component. Since the components are being dynamically created, we use render. Read the docs for more info: https://reactrouter.com/web/api/Route/render-func
+
+  // Just like in the routes if we want the dynamically rendered component to have access to the Router props, we have to pass `props` as an argument.
   return <Route {...rest} render={(props) => routeChecker(props)} />;
 };
 
@@ -28,26 +35,32 @@ export default function Routes({ user }) {
           exact
           path='/pin-details'
           component={() => <PinDetails user={user} />}
+          // since we are checking if a user is authed, we have to pass the user as a props to Private Route so that it can determine if the route should be rendered or redirected. We do this in every route that uses Private Route
+          user={user}
         />
         <PrivateRoute
           exact
           path='/pins'
           component={() => <Pins user={user} />}
+          user={user}
         />
         <PrivateRoute
           exact
           path='/pin-form'
           component={() => <PinForm user={user} />}
+          user={user}
         />
         <PrivateRoute
           exact
           path='/boards/:id'
           component={(props) => <SingleBoard user={user} {...props} />}
+          user={user}
         />
         <PrivateRoute
           exact
           path='/search/:term/:type'
           component={(props) => <SearchResults {...props} />}
+          user={user}
         />
         <PrivateRoute
           exact
